07b44398e9639cb4cd697c23be2027de0b491cdc
Nikita-3013 <nikki@DESKTOP-O3UFAEG.localdomain>
2020-10-21 10:58:42 +0530

    holav1.c

---
+++ holav1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char name;
+    printf("Enter ur name: ");
+    scanf("%s",name);
+    printf("Hola %s mucha gustos",name);//hello in spanish
+    printf(" gracias");
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+//sum of 2 numbers
+#include<stdio.h>
+int main(){
+    int d,e;
+    printf("Enter no1: ");
+    scanf("%d",&d);
+    printf("Enter no2: ");
+    scanf("%d",&e);
+    int c=d+e;
+    printf("sum is : %d",c);
+}
